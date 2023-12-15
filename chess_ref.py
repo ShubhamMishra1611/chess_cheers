668c18f76d6be2b6afe0c28de5623c4b3bd12819
ShubhamMishra1611 <mishra.39@iitj.ac.in>
2023-12-15 19:43:27 +0530

    valid moves function defined and implemented for king and rook, no code for stopping any invalid rule

--- chess_ref.py
+++ chess_ref.py
@@ -20,6 +20,10 @@ class ChessGame:
         self.GRAY = (128, 128, 128)
         self.GREEN = (115, 144, 83)
         self.L_GREEN = (230, 231, 202)
+        self.BLUE = (0, 0, 223)
+        self.RED = (223, 15, 0)
+
+        self.player_side = 0 # 0 : white and 1 : black
 
         # Set the font for the text
         self.font = pygame.font.Font(None, 36)
@@ -31,7 +35,7 @@ class ChessGame:
                                 ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'], 
                                 [0, 0, 0, 0, 0, 0, 0, 0], 
                                 [0, 0, 0, 0, 0, 0, 0, 0], 
-                                [0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 'k', 0, 0, 0], 
                                 [0, 0, 0, 0, 0, 0, 0, 0], 
                                 ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], 
                                 ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']]
@@ -45,6 +49,7 @@ class ChessGame:
         self.piece_file_dict = None
         with open('piece_dict.json') as f:
             self.piece_file_dict = json.load(f)
+        self.piece_file_dict = self.piece_file_dict[str(self.player_side)]
 
         # Set the side the user is playing
         self.USER_SIDE = 0  # {0: White, 1: Black}
@@ -75,7 +80,13 @@ class ChessGame:
         for row in range(8):
             for col in range(8):
                 pygame.draw.rect(
-                    self.screen, self.board[row][col], [col * self.WIDTH / 8, row * self.HEIGHT / 8, self.WIDTH / 8, self.HEIGHT / 8]
+                    self.screen, self.board[row][col], 
+                    [
+                        col * self.WIDTH / 8, 
+                        row * self.HEIGHT / 8, 
+                        self.WIDTH / 8, 
+                        self.HEIGHT / 8
+                    ]
                 )
 
         for row in range(8):
@@ -135,9 +146,99 @@ class ChessGame:
                                     (rect[0]+self.WIDTH/16, rect[1]+self.WIDTH/16),
                                     5)
 
-            
+    def recommend_valid_moves(self, screen, selected_piece):
+        # this function will return x, y of valid moves
+        if selected_piece is None:
+            return None
 
+        piece, x, y = selected_piece
+        moves = [] # see what is moves it is nothing but a list of valid moves. Now question is what is move, it is nothing
+                   # but a tuple of (x, y, 1|0) x and y are coordinate while 1|0 determine if this moves takes and takes and takes and takes
+        if piece.upper() == 'R':
+            # rook is very simple so yeah
+            # move it to the right
+            for i in range(x+1, 8):
+                if self.board_piece_pos[y][i] == 0:
+                    moves.append((i, y, 0))
+                elif self.board_piece_pos[y][i].isupper() != piece.isupper(): # riding on enemy 😏
+                    moves.append((i, y, 1))
+                    break
+                elif self.board_piece_pos[y][i].isupper() == piece.isupper():
+                    break
+            # move it to the left
+            for i in range(x-1, -1, -1):
+                if self.board_piece_pos[y][i] == 0:
+                    moves.append((i, y, 0))
+                elif self.board_piece_pos[y][i].isupper() != piece.isupper(): 
+                    moves.append((i, y, 1))
+                    break
+                elif self.board_piece_pos[y][i].isupper() == piece.isupper():
+                    break
+            # move it to the top
+            for i in range(y+1, 8):
+                if self.board_piece_pos[i][x] == 0:
+                    moves.append((x, i, 0))
+                elif self.board_piece_pos[i][x].isupper() != piece.isupper():
+                    moves.append((x, i, 1))
+                    break
+                elif self.board_piece_pos[i][x].isupper() == piece.isupper():
+                    break
+            # move it to the bottom
+            for i in range(y-1, -1, -1):
+                if self.board_piece_pos[i][x] == 0:
+                    moves.append((x, i, 0))
+                elif self.board_piece_pos[i][x].isupper() != piece.isupper():
+                    moves.append((x, i, 1))
+                    break
+                elif self.board_piece_pos[i][x].isupper() == piece.isupper():
+                    break
+            return moves
+        elif piece.upper() == 'B':
+            pass
+        elif piece.upper() == 'N':
+            pass
+        elif piece.upper() == 'K':
+            # king is the weakest guy, you lose him, you lose it.
+            # anyways so make the guy move everywhere and see where he can go, I will leave checkmate and castle for now
+            # TODO: look for checkmate and castle
+            moves_pos = [
+                [y+1, x],
+                [y+1, x+1],
+                [y+1, x-1],
+                [y-1, x],
+                [y-1, x+1],
+                [y-1, x-1],
+                [y, x-1],
+                [y, x+1],
+            ]
+            for move in moves_pos:
+                if self.board_piece_pos[move[0]][move[1]] == 0:
+                    moves.append((move[1], move[0], 0))
+                elif self.board_piece_pos[move[0]][move[1]].isupper() != piece.isupper():
+                    moves.append((move[1], move[0], 1))
+                elif self.board_piece_pos[move[0]][move[1]].isupper() == piece.isupper():
+                    continue
+            return moves
+        elif piece.upper() == 'Q':
+            pass
+        elif piece.upper() == 'P':
+            # implement only simple front move
 
+            pass
+        return None
+
+    def draw_valid_moves(self, moves):
+        if moves is not None:
+            for x, y, i in moves:
+                pygame.draw.circle(
+                    self.screen,
+                    self.BLUE if i == 0 else self.RED,
+                    (
+                        self.BOARD_POS[0] + x * self.WIDTH / 8 + self.WIDTH / 16, 
+                        self.BOARD_POS[1] + y * self.HEIGHT / 8 + self.HEIGHT / 16
+                    ),
+                    10
+                )
 
     def run(self):
         selected_piece = None
@@ -149,7 +250,7 @@ class ChessGame:
                     
 
                 elif event.type == MOUSEBUTTONUP:
-                    cur_piece, cur_x, cur_y = self.get_square_under_mouse()
+                    _, cur_x, cur_y = self.get_square_under_mouse()
                     if selected_piece:
                         piece, x, y = selected_piece
                         if x != cur_x or  y!=cur_y:
@@ -162,19 +263,21 @@ class ChessGame:
                     piece, x, y = self.get_square_under_mouse()
                     if piece!=0 and selected_piece is None:
                         selected_piece = (piece, x, y)
-                    elif not (x!=selected_piece[1] or y!=selected_piece[2]):
-                        selected_piece = None
+                    elif selected_piece is not None: 
+                        if not (x!=selected_piece[1] or y!=selected_piece[2]):
+                            selected_piece = None
                             
                 
                 elif event.type == MOUSEMOTION:
                     pass
 
-            # piece, x, y = self.get_square_under_mouse()
             self.screen.fill(self.WHITE)
             self.make_UI_and_place_piece()
             if selected_piece:
                 self.draw_selected(self.screen, selected_piece[0], selected_piece[1], selected_piece[2])
                 self.draw_dragging_cursor(self.screen, self.board_piece_pos, selected_piece)
+                moves = self.recommend_valid_moves(self.screen, selected_piece)
+                self.draw_valid_moves(moves)
             else:
                 _, x, y = self.get_square_under_mouse()
                 if x is not None :
